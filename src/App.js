97bd5452758b20757afb9d1168d76a2082447299
Iyobosa Omoruyi <omosiyobo@gmail.com>
2022-08-24 10:15:45 +0100

    homepage done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import NavBar from "./components/Navbar";
 import Register from "./page/Register";
 
 import Footer from "./components/Footer";
+import { Home } from './page/Home';
+import { About } from './page/About';
+import { Schedule } from './page/Schedule';
+import { Speakers } from './page/Speakers';
+import { Sponsors } from './page/Sponsors';
 
 const App = () => {
 
@@ -16,7 +21,19 @@ const App = () => {
         
       <NavBar/>
       
-        <Routes><Route path='signup' element = {<Register />}></Route></Routes>
+        <Routes>
+        <Route path='/' element = {
+          <>
+            <Home/>
+            <About/>
+            <Speakers/>
+            <Schedule/>
+            <Sponsors/>
+          </>}>
+        </Route>
+          <Route path='signup' element = {<Register />}></Route>  
+        </Routes>
+        
         
         <Footer />
         
---
+++ src/page/Sponsors.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Sponsors = () => {
+  return (
+    <div className="sponsors">
+      <h5>SPONSORSHIP</h5>
+      <h3>OFFICIAL SPONSORS OF EVENT</h3>
+    </div>
+  )
+}
