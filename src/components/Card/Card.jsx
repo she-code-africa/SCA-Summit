98adbbaccbde2bad027de17b15352a8639d12cf7
EseAlli <essealli.ea@gmail.com>
2022-08-27 22:54:19 +0100

    speakers page redesign, card component folder restucture

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Card.css";
+
+export const Card = (props) => {
+  return (
+    <div className="cd">
+      <img src={props.pic} alt="Avatar" />
+      <div className="cont">
+        <p>Speaker</p>
+        <h2>{props.name}</h2>
+        <p>{props.title}</p>
+      </div>
+    </div>
+  );
+};
