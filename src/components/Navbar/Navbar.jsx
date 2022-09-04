28db4dc51a904b1eb0c3f43993c2e31d5d72b320
yasira <yasirayusif@gmail.com>
2022-09-04 09:49:23 -0700

    hackathon navbar background changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import { useLocation } from 'react-router-dom';
+
 import {
   Button,
   Navbar,
@@ -16,6 +18,9 @@ import logoImage from "../../assets/image/logo-inverted.png";
 import "./Navbar.css";
 
 const NavBar = () => {
+
+  // use location for redirecting and changing of navbar color on a diff page 
+  const location = useLocation();
 
   // use state for changing background color when a user scroll through 
   const [colorChange, setColorchange] = useState(false);
@@ -34,11 +39,18 @@ const NavBar = () => {
 
   window.addEventListener("scroll", changeNavbarColor);
 
+  // works the same way as the previous one 
+  // useEffect(() => {
+  //   changeNavbarColor()
+  // window.addEventListener("scroll", changeNavbarColor);
+
+  // })
+
   // toggling hamburger menu 
   const toggle = () => setClick(!click);
-
+  
   return (
-    <Navbar className="navbar" expand="md" id={colorChange ? "navbarBg" : ""}>
+    <Navbar className= {location.pathname=== "/hackathon"? "navbarhackathon" :"navbar"} expand="md" id={colorChange ? "navbarBg" : ""}>
       <NavbarBrand>
         <img src={logoImage} alt={logoImage} />
       </NavbarBrand>
@@ -46,7 +58,7 @@ const NavBar = () => {
       <Collapse isOpen={click} navbar>
         <Nav className="m-auto nav-items" navbar>
           <NavItem>
-            <NavLink href="#" className="text-light">
+            <NavLink href="/" className="text-light">
               Home
             </NavLink>
           </NavItem>
