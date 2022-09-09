ae079dee28b0bfe5acc63d0095cd918348b4f09f
EseAlli <essealli.ea@gmail.com>
2022-09-09 22:49:57 +0100

    edited about section content

--- src/page/About/About.jsx
+++ src/page/About/About.jsx
@@ -9,20 +9,17 @@ export const About = () => {
         <h5>ABOUT EVENT</h5>
         <h3>THE LARGEST COMMUNITY OF WOMEN IN TECH SUMMIT</h3>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-          dolorum, voluptatum optio provident nesciunt dignissimos ipsum, sed
-          corporis reiciendis cumque officia. Quisquam similique quos temporibus
-          modi cum unde recusandae. Nihil hic eveniet assumenda soluta molestias
-          aut rem quam eius consequuntur odio harum facere minus provident
-          facilis obcaecati, quasi beatae dolorem eaque tempora neque suscipit?
-          Quos deserunt aliquam reiciendis qui quo ducimus dolor numquam
-          adipisci repellat? Illo esse vero ea quisquam.
+          Our first edition of what we intend to make our annual headline
+          conference this year aims to change that. Our goal is to gather our
+          members from different states in Nigeria and neighbouring African
+          countries for a hybrid summit themed "Elevating the African Woman in
+          Tech". We plan to host 1,000 women participants for the first edition.
         </p>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, quod!
-          Soluta totam itaque neque. Asperiores animi possimus reprehenderit
-          suscipit ut optio nesciunt, tenetur libero, voluptatum et veritatis
-          cupiditate eos sunt.
+          Our goal is to create a space safe where these women attendees can
+          have different empowering conversations and training on different
+          skills/career fields in tech while networking with notable women in
+          the tech industry and paterning Companies.
         </p>
       </div>
       <img src={deco} alt="deco" />
