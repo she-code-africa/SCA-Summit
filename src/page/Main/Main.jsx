8469651360303e91a3fb1a34d081d40e8d2fdda6
EseAlli <essealli.ea@gmail.com>
2022-09-10 14:36:03 +0100

    code clean up

--- src/page/Main/Main.jsx
+++ src/page/Main/Main.jsx
@@ -1,7 +1,7 @@
 import { Home } from "../Home/Home";
 import { Attendees } from "../Attendees/Attendees";
 import { About } from "../About/About";
-import { Schedule } from "../Schedule/Schedule";
+// import { Schedule } from "../Schedule/Schedule";
 import { Speakers } from "../Speakers/Speakers";
 import { Sponsors } from "../Sponsors/Sponsors";
 import Hackathon from "../Hackathon/Hackathon";
