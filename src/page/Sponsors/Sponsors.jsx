63cd33ceeb61600699f6370460240a77106b8849
EseAlli <essealli.ea@gmail.com>
2022-09-11 16:18:20 +0100

    sponsors section responsiveness

--- src/page/Sponsors/Sponsors.jsx
+++ src/page/Sponsors/Sponsors.jsx
@@ -8,26 +8,26 @@ export const Sponsors = () => {
     <div className="sponsors">
       <h5>SPONSORSHIP</h5>
       <h3>OFFICIAL SPONSORS OF EVENT</h3>
+      <p>PREMIUM SPONSORS</p>
       <div>
-        <p>PREMIUM SPONSORS</p>
         <img src={google} alt="google-logo" />
         <img src={tetra} alt="tetra-logo" />
         <img src={google} alt="google-logo" />
         <img src={tetra} alt="tetra-logo" />
       </div>
+      <p>GOLD SPONSORS</p>
       <div>
-        <p>GOLD SPONSORS</p>
         <img src={google} alt="google-logo" />
         <img src={tetra} alt="tetra-logo" />
         <img src={google} alt="google-logo" />
       </div>
+      <p>SILVER SPONSORS</p>
       <div>
-        <p>SILVER SPONSORS</p>
         <img src={google} alt="google-logo" />
         <img src={tetra} alt="tetra-logo" />
       </div>
+      <p>BRONZE SPONSORS</p>
       <div>
-        <p>BRONZE SPONSORS</p>
         <img src={tetra} alt="tetra-logo" />
         <img src={google} alt="google-logo" />
       </div>
