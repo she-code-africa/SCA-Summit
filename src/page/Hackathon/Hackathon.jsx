4b27ae15ba5d99eed50da486e3397e12aef5cb38
EseAlli <essealli.ea@gmail.com>
2022-09-13 17:05:58 +0100

    added hackathon content and mobile responsiveness bug fix

--- src/page/Hackathon/Hackathon.jsx
+++ src/page/Hackathon/Hackathon.jsx
@@ -20,30 +20,53 @@ const Hackathon = () => {
       </div>
 
       <div className="shecode-hackathon-text">
-        <h2>She Code Africa Hackathon</h2>
-
-        <span>
-          2022: <span>Gamification</span>
-        </span>
+        <h2>
+          She Code Africa Hackathon 2022: <span>Gamification</span>
+        </h2>
 
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil quas
-          maxime cupiditate magnam sed id ipsum, sequi sit aliquid quo porro
-          mollitia quidem praesentium, a facere saepe tempora neque ad?
+          SCA HackFest aims to be an annual open source hackathon organized by
+          She Code Africa dedicated to building and improving open source
+          projects that serve people and solve problems around the world. We'll
+          be calling on female hackers who identify as African women in tech, to
+          join us for a month, to work on open-source projects to solve
+          challenging problems.
+          <br />
+          <br />
+          All solutions built will be released as open source projects so they
+          can be of use and contributed worldwide.
         </p>
 
-        <ul>
+        <h6 className="text-start">Objectives</h6>
+        <ul className="lh-lg">
           <li>
-            {" "}
-            <span className="start-date"> Start date:</span>{" "}
-            <span className="date">October 20-21, 2022</span>
+            Support open source projects solving prevalent problems globally
+            with special preference for open source projects supporting
+            underrepresented communities.
           </li>
-
           <li>
-            <span className="start-date">Application deadline: </span>
-            <span className="date">October 12, 2022</span>
+            Bring open source projects supporting underrepresented groups into
+            the limelight.
+          </li>
+          <li>
+            Encourage more diversity and inclusion within the open source
+            community through the female hackers' contributions.
           </li>
         </ul>
+
+        <h6 className="text-start">Benefits</h6>
+        <ul>
+          <li>Explore new technologies</li>
+          <li>Drive innovation</li>
+          <li>Create solutions for social good</li>
+          <li>Reward innovative thinking</li>
+        </ul>
+        <p className="fw-bold lh-base">
+          Duration: October 10, 2022 - October 31st, 2022
+        </p>
+        <p className="fw-bold lh-base">
+          Application deadline: October 12, 2022
+        </p>
 
         <Button className="register-btn p-2">Apply now</Button>
       </div>
