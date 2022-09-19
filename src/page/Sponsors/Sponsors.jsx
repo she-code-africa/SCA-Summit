ecf9371cb3be45dff8537caf0e8a2aef230f9c55
EseAlli <essealli.ea@gmail.com>
2022-09-19 21:16:41 +0100

    added hackathon and gold sponsors

--- src/page/Sponsors/Sponsors.jsx
+++ src/page/Sponsors/Sponsors.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 import "./Sponsors.css";
-import google from "../../assets/image/google-logo.svg";
-import tetra from "../../assets/image/tetra.svg";
+// import google from "../../assets/image/google-logo.svg";
+// import tetra from "../../assets/image/tetra.svg";
+import oseq from "../../assets/image/OSEQ-logo.png";
+import zaycodes from "../../assets/image/Zaycodes-white.jpg";
+import yellowCard from "../../assets/image/yellowcard-logo.png";
 
 export const Sponsors = () => {
   return (
     <section className="sponsors">
       <h5>SPONSORSHIP</h5>
       <h3>OFFICIAL SPONSORS OF EVENT</h3>
-      <p>PREMIUM SPONSORS</p>
+      <p>HACKATHON SPONSORS</p>
+      <div>
+        <img src={oseq} alt="OSEQ-logo" />
+        <img src={zaycodes} alt="zaycodes-logo" />
+      </div>
+      {/* <p>PREMIUM SPONSORS</p>
       <div>
         <img src={google} alt="google-logo" />
         <img src={tetra} alt="tetra-logo" />
         <img src={google} alt="google-logo" />
         <img src={tetra} alt="tetra-logo" />
-      </div>
+      </div> */}
       <p>GOLD SPONSORS</p>
       <div>
-        <img src={google} alt="google-logo" />
-        <img src={tetra} alt="tetra-logo" />
-        <img src={google} alt="google-logo" />
+        <img src={yellowCard} alt="yellowcard-logo" />
+
+        <img src={oseq} alt="OSEQ-logo" />
       </div>
-      <p>SILVER SPONSORS</p>
+      {/* <p>SILVER SPONSORS</p>
       <div>
         <img src={google} alt="google-logo" />
         <img src={tetra} alt="tetra-logo" />
@@ -30,7 +38,7 @@ export const Sponsors = () => {
       <div>
         <img src={tetra} alt="tetra-logo" />
         <img src={google} alt="google-logo" />
-      </div>
+      </div> */}
     </section>
   );
 };
