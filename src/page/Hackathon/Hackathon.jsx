2fda75ae4a0a52d1eb8ec2e7b4c253eee078ced2
EseAlli <essealli.ea@gmail.com>
2022-09-20 17:49:59 +0100

    added more info to hackathon section

--- src/page/Hackathon/Hackathon.jsx
+++ src/page/Hackathon/Hackathon.jsx
@@ -5,22 +5,20 @@ import "./Hackathon.css";
 const Hackathon = () => {
   return (
     <section className="hackathon-container" id="hackathon">
-      <div className="hackathon-images">
-        <img src={HackathonImg} id="hackathon-img" alt={HackathonImg} />
-
-        <img
-          src={HackathonImg1}
-          alt={HackathonImg1}
-          className="hackathon-img-2"
-        />
-      </div>
-
       <div className="shecode-hackathon-text">
-        <h2>
+        <h2 className="text-center">
           She Code Africa Hackathon 2022: <span>Gamification</span>
         </h2>
+        <div className="hackathon-images">
+          <img src={HackathonImg} id="hackathon-img" alt={HackathonImg} />
 
-        <p>
+          <img
+            src={HackathonImg1}
+            alt={HackathonImg1}
+            className="hackathon-img-2"
+          />
+        </div>
+        <p class="hackathon-info">
           SCA HackFest aims to be an annual open source hackathon organized by
           She Code Africa dedicated to building and improving open source
           projects that serve people and solve problems around the world. We'll
@@ -28,10 +26,11 @@ const Hackathon = () => {
           join us for a month, to work on open-source projects to solve
           challenging problems.
           <br />
-          <br />
           All solutions built will be released as open source projects so they
           can be of use and contributed worldwide.
         </p>
+
+        <br />
 
         <h6 className="text-start">Objectives</h6>
         <ul className="objectives">
@@ -56,6 +55,25 @@ const Hackathon = () => {
           <li>Drive innovation</li>
           <li>Create solutions for social good</li>
           <li>Reward innovative thinking</li>
+        </ul>
+
+        <h6>All Participants get data stipends and SCA Swag and</h6>
+        <ul>
+          <li>First Place Cash Prize: N500,000</li>
+          <li>Second Place Cash Prize: N300,000</li>
+          <li>Third Place Cash Prize: N200,000</li>
+        </ul>
+
+        <h6>Other Possible Prizes</h6>
+        <ul>
+          <li>
+            Chance to receive external internship opportunities, and
+            scholarships from sponsors or externals.
+          </li>
+          <li>
+            Internship opportunities with other organizations. (Subject to
+            hackers’ solutions standard)
+          </li>
         </ul>
         <p className="fw-bold lh-base">
           Duration: October 10, 2022 - October 31st, 2022
