7a2c6ba5c34c2312e2af2dcf3342732a11707feb
EseAlli <essealli.ea@gmail.com>
2022-09-23 10:52:39 +0100

    fixed typos, changed hackathon images

--- src/page/About/About.jsx
+++ src/page/About/About.jsx
@@ -11,11 +11,11 @@ export const About = () => {
       <h2>Elevating The African Women in Tech</h2>
       <div className="about-content">
         <blockquote>
-          Our first edition of what we intend to make our annual headline
-          conference this year aims to change that. Our goal is to gather our
-          members from different states in Nigeria and neighbouring African
-          countries for a hybrid summit themed "Elevating the African Woman in
-          Tech". We plan to host 1,000 women participants for the first edition.
+          The SCA Summit 2022 is the maiden edition of what we intend to make
+          our headline conference. Our goal is to gather our members from
+          different states in Nigeria and neighbouring African countries for a
+          hybrid summit themed "Elevating the African Woman in Tech". We plan to
+          host 1,000 women participants for the first edition.
           <br />
           <br />
           Our goal is to create a space safe where these women attendees can
--- src/page/Hackathon/Hackathon.jsx
+++ src/page/Hackathon/Hackathon.jsx
@@ -1,95 +1,91 @@
-import HackathonImg from "../../assets/image/hackathon.png";
-import HackathonImg1 from "../../assets/image/hackathon2png.png";
+import hackfest from "../../assets/image/hackfest.png";
+import hackathon from "../../assets/image/hack-fest.png";
 import "./Hackathon.css";
 
 const Hackathon = () => {
   return (
     <section className="hackathon-container" id="hackathon">
-      <div className="shecode-hackathon-text">
-        <h2 className="text-center">
-          She Code Africa Hackathon 2022: <span>Gamification</span>
-        </h2>
+      <h2 className="text-center">
+        SCA <span>Hackfest</span> 2022: Celebrating Her Contribution in Tech
+      </h2>
+      <div className="hackathon">
         <div className="hackathon-images">
-          <img src={HackathonImg} id="hackathon-img" alt={HackathonImg} />
+          <img src={hackfest} id="hackathon-img" alt="SCA-hackfest" />
 
-          <img
-            src={HackathonImg1}
-            alt={HackathonImg1}
-            className="hackathon-img-2"
-          />
+          <img src={hackathon} alt={hackathon} className="hackathon-img-2" />
         </div>
-        <p class="hackathon-info">
-          SCA HackFest aims to be an annual open source hackathon organized by
-          She Code Africa dedicated to building and improving open source
-          projects that serve people and solve problems around the world. We'll
-          be calling on female hackers who identify as African women in tech, to
-          join us for a month, to work on open-source projects to solve
-          challenging problems.
-          <br />
-          All solutions built will be released as open source projects so they
-          can be of use and contributed worldwide.
-        </p>
+        <div className="shecode-hackathon-text">
+          <p class="hackathon-info">
+            SCA HackFest aims to be an annual open source hackathon organized by
+            She Code Africa dedicated to building and improving open source
+            projects that serve people and solve problems around the world.
+            We'll be calling on female hackers who identify as African women in
+            tech, to join us for a month, to work on open-source projects to
+            solve challenging problems.
+            <br />
+            All solutions built will be released as open source projects so they
+            can be of use and contributed worldwide.
+          </p>
 
-        <br />
+          <h6 className="text-start">Objectives</h6>
+          <ul className="objectives">
+            <li>
+              Support open source projects solving prevalent problems globally
+              with special preference for open source projects supporting
+              underrepresented communities.
+            </li>
+            <li>
+              Bring open source projects supporting underrepresented groups into
+              the limelight.
+            </li>
+            <li>
+              Encourage more diversity and inclusion within the open source
+              community through the female hackers' contributions.
+            </li>
+          </ul>
 
-        <h6 className="text-start">Objectives</h6>
-        <ul className="objectives">
-          <li>
-            Support open source projects solving prevalent problems globally
-            with special preference for open source projects supporting
-            underrepresented communities.
-          </li>
-          <li>
-            Bring open source projects supporting underrepresented groups into
-            the limelight.
-          </li>
-          <li>
-            Encourage more diversity and inclusion within the open source
-            community through the female hackers' contributions.
-          </li>
-        </ul>
+          <h6 className="text-start">Benefits</h6>
+          <ul>
+            <li>Explore new technologies</li>
+            <li>Drive innovation</li>
+            <li>Create solutions for social good</li>
+            <li>Reward innovative thinking</li>
+          </ul>
 
-        <h6 className="text-start">Benefits</h6>
-        <ul>
-          <li>Explore new technologies</li>
-          <li>Drive innovation</li>
-          <li>Create solutions for social good</li>
-          <li>Reward innovative thinking</li>
-        </ul>
+          <h6 className="text-start">
+            Our top three teams get data stipends, SCA swag and
+          </h6>
+          <ul>
+            <li>First Place Cash Prize: N500,000</li>
+            <li>Second Place Cash Prize: N300,000</li>
+            <li>Third Place Cash Prize: N200,000</li>
+          </ul>
 
-        <h6 className="text-start">
-          All Participants get data stipends and SCA Swag and
-        </h6>
-        <ul>
-          <li>First Place Cash Prize: N500,000</li>
-          <li>Second Place Cash Prize: N300,000</li>
-          <li>Third Place Cash Prize: N200,000</li>
-        </ul>
+          <h6 className="text-start">Other Possible Prizes</h6>
+          <ul>
+            <li>
+              Chance to receive external internship opportunities, and
+              scholarships from sponsors or externals.
+            </li>
+            <li>
+              Internship opportunities with other organizations. (Subject to
+              hackers’ solutions standard)
+            </li>
+          </ul>
+          <p className="fw-bold lh-base">
+            Duration: October 10, 2022 - October 31st, 2022
+          </p>
+          <p className="fw-bold lh-base">
+            Application deadline: October 12, 2022
+          </p>
 
-        <h6 className="text-start">Other Possible Prizes</h6>
-        <ul>
-          <li>
-            Chance to receive external internship opportunities, and
-            scholarships from sponsors or externals.
-          </li>
-          <li>
-            Internship opportunities with other organizations. (Subject to
-            hackers’ solutions standard)
-          </li>
-        </ul>
-        <p className="fw-bold lh-base">
-          Duration: October 10, 2022 - October 31st, 2022
-        </p>
-        <p className="fw-bold lh-base">
-          Application deadline: October 12, 2022
-        </p>
-
-        <a
-          className="register-btn p-2 text-center"
-          href="https://docs.google.com/forms/d/e/1FAIpQLSfsMX2ZRdf0uXkLJZZxiBgE-g8JHupLv_cc9IqH9_ch4U2PIA/viewform"
-        >
-          Apply now
-        </a>
+          <a
+            className="register-btn p-2 text-center"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfsMX2ZRdf0uXkLJZZxiBgE-g8JHupLv_cc9IqH9_ch4U2PIA/viewform"
+          >
+            Apply now
+          </a>
+        </div>
       </div>
     </section>
   );
