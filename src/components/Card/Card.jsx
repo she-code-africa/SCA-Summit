0f2a401c1850858c08d19fd9b90d5b9019e4c482
EseAlli <essealli.ea@gmail.com>
2022-10-23 09:20:12 +0100

    added speakers photo and adjusted card styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,11 +4,12 @@ import "./Card.css";
 export const Card = (props) => {
   return (
     <div className="cd">
-      <img src={props.pic} alt="Avatar" />
+      <img src={props.pic} alt="Avatar"/>
       <div className="cont">
-        {/* <p>Speaker</p> */}
         <h2>{props.name}</h2>
         <p>{props.title}</p>
+        <p>{props.role}</p>
+        {/* <p></p> */}
       </div>
     </div>
   );
--- src/page/Speakers/Speakers.jsx
+++ src/page/Speakers/Speakers.jsx
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
 import { Card } from "../../components/Card/Card";
-// import { faker } from "@faker-js/faker";
 import "./Speakers.css";
-import placeholder from "../../assets/image/avatar.png";
+import odun from "../../assets/image/Odunayo_Eweniyi.JPG";
+import ashley from "../../assets/image/Ashley-Immanuel.jpg";
+import esther from "../../assets/image/Esther-Olamoyegun.jpg";
+import mbaoma from "../../assets/image/Mbaoma-Chioma-Mary.jpg";
+import adebayo from "../../assets/image/Esther-Temi-Adebayo.jpeg";
+import folashade from "../../assets/image/Folashade-Daini.jpeg";
+import elizabeth from "../../assets/image/Elizabeth-Okaome.jpeg";
+import karen from "../../assets/image/Karen-Chukwu.JPG";
+import princess from "../../assets/image/Princess-Egbuna-Oluebube.jpeg";
+import paloma from "../../assets/image/Paloma-Peter-Dzoho.png";
+import oreoluwa from "../../assets/image/Oreoluwa-Lesi.jpeg";
+import koromone from "../../assets/image/Koromone-Koroye.jpg";
+import doyin from "../../assets/image/Dedoyin-Ajayi.png"
 export const Speakers = () => {
   const [isShown, setIsShown] = useState(false);
 
@@ -17,68 +28,149 @@ export const Speakers = () => {
       <div className="speakers">
         <Card
           className="itm"
-          name="TBD"
-          // title={faker.name.jobTitle()}
-          pic={placeholder}
+          name="Odunayo Eweniyi"
+          title="COO & Co-founder, Piggyvest"
+          pic={odun}
+          role="Keynote Speaker"
         />
 
         <Card
           className="itm"
-          name="TBD"
-          // title={faker.name.jobTitle()}
-          pic={placeholder}
+          name="Ashley Immanuel "
+          title="COO & Co-founder, Semicolon"
+          pic={ashley}
+          role="Keynote Speaker"
         />
 
         <Card
           className="itm"
-          name="TBD"
-          // title={faker.name.jobTitle()}
-          pic={placeholder}
+          name="Esther Olamoyegun"
+          title="Content Marketer & Writer"
+          pic={esther}
+          role="Panelist"
         />
 
         <Card
           className="itm"
-          name="TBD"
-          // title={faker.name.jobTitle()}
-          pic={placeholder}
+          name="Mbaoma Chioma Mary"
+          title="Software Engineer & Technical Writer"
+          pic={mbaoma}
+          role="Panelist"
         />
 
         <Card
           className="itm"
-          name="TBD"
-          // title={faker.name.jobTitle()}
-          pic={placeholder}
+          name="Esther Adebayo"
+          role="Panelist"
+          title="Technical Recruiter, Decagon"
+          pic={adebayo}
         />
 
         <Card
           className="itm"
-          name="TBD"
-          // title={faker.name.jobTitle()}
-          pic={placeholder}
+          name="Oluebube Princess Egbuna"
+          title="Engineering Lead, Well Beyond"
+          role="Panelist"
+          pic={princess}
         />
 
         {isShown && (
           <>
             <Card
               className="itm"
-              name="TBD"
-              // title={faker.name.jobTitle()}
+              name="Folashade Daini"
+              title="Content Strategist, LazerPay"
+              role="Moderator"
+              pic={folashade}
+            />
+
+            <Card
+              className="itm"
+              name="Oreoluwa Lesi"
+              title="Founder, W.TEC"
+              role="Panelist"
+              pic={oreoluwa}
+            />
+
+            <Card
+              className="itm"
+              name="Koromone 'KK' Koroye"
+              title="Managing Editor, TechCabal"
+              role="Panelist"
+              pic={koromone}
+            />
+
+            <Card
+              className="itm"
+              name="Elizabeth Okaome"
+              title="Developer Relations Manager, Unicorn & Founder, EmpowerHer Community"
+              role="Panelist"
+              pic={elizabeth}
+            />
+
+            <Card
+              className="itm"
+              name="Karen Chukwu"
+              title="Managing Partner, Katwig & Dale"
+              role="Legal Backup for Women in Tech"
+              pic={karen}
+            />
+
+            <Card
+              className="itm"
+              name="Dedoyin Ajayi"
+              title="Psychotherapist & Mental Health Expert"
+              role="Mental Health for Techies"
+              pic={doyin}
+            />
+
+            <Card
+              className="itm"
+              name="Paloma Peter-Dzoho"
+              title="Software Developer & Technical Community Manager"
+              role="How to Gain the Most from Communities as a Contributor"
+              pic={paloma}
+            />
+
+            {/* <Card
+              className="itm"
+              name="Chidinma Nwanya "
+              title="Managing Partner, Katwig & Dale"
+              role="Panelist"
               pic={placeholder}
             />
 
             <Card
               className="itm"
-              name="TBD"
-              // title={faker.name.jobTitle()}
+              name="Chika"
+              title="Managing Partner, Katwig & Dale"
+              role="Panelist"
               pic={placeholder}
             />
 
             <Card
               className="itm"
-              name="TBD"
-              // title={faker.name.jobTitle()}
+              name="Ngozi"
+              title="Managing Partner, Katwig & Dale"
+              role="Panelist"
               pic={placeholder}
             />
+
+            <Card
+              className="itm"
+              name="Chidinma Nwanya "
+              title="Managing Partner, Katwig & Dale"
+              role="Panelist"
+              pic={placeholder}
+            />
+
+            <Card
+              className="itm"
+              name="Chidinma Nwanya "
+              title="Managing Partner, Katwig & Dale"
+              role="Panelist"
+              pic={placeholder}
+            /> */}
           </>
         )}
       </div>
