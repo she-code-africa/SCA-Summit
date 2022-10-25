a8a5c52827b45ea333c1659c97daae9a5c84defc
elizabeth <leezeelola@gmail.com>
2022-10-25 13:01:05 +0100

    add modal

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,49 @@
-import React, { useContext } from "react";
+import React, { useRef, useEffect } from "react";
+import { CloseCircleOutline } from 'react-ionicons'
 import "./Card.css";
-import Context from "../../context/context";
 
 export const Card = (props) => {
-  const { setOpenModal, openModal } = useContext(Context);
+  const showModal = useRef()
+  const hideModal = useRef()
+  const modal = useRef()
+
+  useEffect(() => {
+    showModal?.current?.addEventListener('click', () => {
+      modal?.current?.showModal()
+    })
+
+    hideModal?.current?.addEventListener('click', () => {
+      modal?.current?.close()
+    })
+  }, [])
+
   return (
-    <div className="cd" onClick={ () => setOpenModal(true)}>
-      <img src={props.pic} alt="Avatar" />
-      <div className="cont">
-        <h2>{props.name}</h2>
-        <p>{props.title}</p>
-        <p>{props.role}</p>
-        {/* <p></p> */}
+    <>
+      <div className="cd" ref={showModal}>
+        <img src={props.pic} alt="Avatar" />
+        <div className="cont">
+          <h2>{props.name}</h2>
+          <p>{props.title}</p>
+          <p>{props.role}</p>
+          {/* <p></p> */}
+        </div>
       </div>
-    </div>
+
+      <dialog id="modal" ref={modal}>
+        <section className="speakers-modal-container">
+          <div className="modal-content">
+            <button ref={hideModal}>
+              <CloseCircleOutline
+                color={'#00000'} 
+                title="close modal"
+                height="30px"
+                width="30px"
+              />
+            </button>
+            Other modal Content here
+          </div>
+        </section>
+      </dialog>
+    </>
   );
 };
