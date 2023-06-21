b02a2400f5bee2f1ccbc6d99caaddf8651f35a82
EseAlli <essealli.ea@gmail.com>
2023-06-21 16:12:53 +0100

    updated gallery image and url

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import sample from "../../assets/images/gallery-sample.png";
-import sample2 from "../../assets/images/sample-img.png";
-import sample3 from "../../assets/images/sample-img2.png";
-import sample4 from "../../assets/images/sample-img3.png";
-import sample5 from "../../assets/images/sample-img4.png";
-import sample6 from "../../assets/images/sample-img5.png";
-import sample7 from "../../assets/images/sample-img6.png";
-import sample8 from "../../assets/images/sample-img7.png";
+import sample from "../../assets/images/gallery-sample.jpg";
+import sample2 from "../../assets/images/sample-img.jpg";
+import sample3 from "../../assets/images/sample-img2.jpg";
+import sample4 from "../../assets/images/sample-img3.jpg";
+import sample6 from "../../assets/images/sample-img5.jpg";
+import sample7 from "../../assets/images/sample-img6.jpg";
+import sample8 from "../../assets/images/sample-img7.jpg";
 import { RxArrowTopRight } from "react-icons/rx";
 
 const Gallery = () => {
@@ -17,22 +16,66 @@ const Gallery = () => {
           ~SCA SUMMIT 2022 HIGHLIGHTS~
         </h3>
         <div className="flex gap-4 overflow-x-scroll no-scrollbar mt-8">
-          <img alt="gallery" src={sample2} className="object-contain" />
-          <div className="flex flex-col justify-between">
-            <img alt="gallery" src={sample3} className="object-contain" />
-            <img alt="gallery" src={sample4} className="object-contain" />
+          <div className="h-fit max-h-[280px] md:max-h-[415px] border-10 border-[12px] md:border-8 border-primary-pink">
+            <img
+              alt="gallery"
+              src={sample2}
+              className="object-cover max-w-sm lg:h-[400px]"
+            />
           </div>
-          <img alt="gallery" src={sample} className="object-contain" />
-
-          <img alt="gallery" src={sample6} className="object-contain" />
-          <div className="flex flex-col justify-between">
-            <img alt="gallery" src={sample7} className="object-contain" />
-            <img alt="gallery" src={sample8} className="object-contain" />
+          <div className="flex flex-row lg:flex-col justify-between md:max-h-[415px] gap-2">
+            <div className="border-8 border-primary-pink max-h-[280px] md:max-h-full">
+              <img
+                alt="gallery"
+                src={sample3}
+                className="object-cover max-w-sm h-[265px] lg:h-[200px]"
+              />
+            </div>
+            <div className="border-8 border-primary-pink h-fit">
+              <img
+                alt="gallery"
+                src={sample4}
+                className="object-cover max-w-sm lg:w-full h-[265px] lg:h-[180px]"
+              />
+            </div>
+          </div>
+          <div className="h-fit max-h-[350px] md:max-h-[415px] border-8 border-primary-pink">
+            <img
+              alt="gallery"
+              src={sample}
+              className="object-cover max-w-[300px] h-[265px] lg:h-[400px]"
+            />
+          </div>
+          <div className="h-fit max-h-[415px] border-8 border-primary-pink">
+            <img
+              alt="gallery"
+              src={sample6}
+              className="object-cover max-w-[300px] h-[265px] lg:h-[400px]"
+            />
+          </div>
+          <div className="flex flex-row lg:flex-col justify-between md:max-h-[415px] gap-2">
+            <div className="border-8 border-primary-pink max-h-[280px] md:max-h-full">
+              <img
+                alt="gallery"
+                src={sample7}
+                className="object-cover max-w-sm lg:w-full h-[265px] lg:h-[180px]"
+              />
+            </div>
+            <div className="border-8 border-primary-pink h-fit">
+              <img
+                alt="gallery"
+                src={sample8}
+                className="object-cover max-w-sm h-[265px] lg:h-[180px]"
+              />
+            </div>
           </div>
         </div>
 
         <div className="justify-self-end mt-4 pr-4 md:pr-8 lg:pr-12">
-          <a href="/" className="flex items-center gap-1">
+          <a
+            href="https://drive.google.com/drive/folders/1tRQkG3jzcST71OOZqlT5_binwsdUQdhm"
+            className="flex items-center gap-1"
+          >
             View All Pictures <RxArrowTopRight color="#B70569" />
           </a>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         "primary-pink": "#B70569",
         "primary-light-pink": "#FFF7FC",
         "secondary-light-pink": "rgba(253, 192, 227, 0.2)",
+        "linear-gradient":
+          "linear-gradient(180deg, #B70569 17.71%, #210D15 81.77%)",
       },
     },
   },
