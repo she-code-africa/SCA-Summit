6e02519ed52b8302aeb48e0b010b1c0d6f1f73e1
EseAlli <essealli.ea@gmail.com>
2023-06-24 12:41:39 +0100

    restyled gallery photos

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -16,56 +16,56 @@ const Gallery = () => {
           ~SCA SUMMIT 2022 HIGHLIGHTS~
         </h3>
         <div className="flex gap-4 overflow-x-scroll no-scrollbar mt-8">
-          <div className="h-fit max-h-[280px] md:max-h-[415px] border-10 border-[12px] md:border-8 border-primary-pink">
+          <div className="h-fit max-h-[280px] md:max-h-[415px] border-4 -mx-3 rounded-[50px] border-primary-pink">
             <img
               alt="gallery"
               src={sample2}
-              className="object-cover max-w-sm lg:h-[400px]"
+              className="object-cover max-w-sm lg:h-[400px] rounded-[50px]"
             />
           </div>
-          <div className="flex flex-row lg:flex-col justify-between md:max-h-[415px] gap-2">
-            <div className="border-8 border-primary-pink max-h-[280px] md:max-h-full">
+          <div className="flex flex-row lg:flex-col justify-between md:justify-normal md:max-h-[415px] gap-2">
+            <div className="border-4 border-[#210D15] rounded-t-3xl max-h-[280px] md:max-h-full">
               <img
                 alt="gallery"
                 src={sample3}
-                className="object-cover max-w-sm h-[265px] lg:h-[200px]"
+                className="object-cover max-w-sm h-[265px] lg:h-[200px] rounded-t-xl"
               />
             </div>
-            <div className="border-8 border-primary-pink h-fit">
+            <div className="border-4 rounded-b-3xl border-primary-pink h-fit">
               <img
                 alt="gallery"
                 src={sample4}
-                className="object-cover max-w-sm lg:w-full h-[265px] lg:h-[180px]"
+                className="object-cover max-w-sm lg:w-full h-[265px] lg:h-[180px] rounded-b-2xl"
               />
             </div>
           </div>
-          <div className="h-fit max-h-[350px] md:max-h-[415px] border-8 border-primary-pink">
+          <div className="h-fit max-h-[350px] md:max-h-[415px] border-4 border-primary-pink rounded-3xl rounded-bl-none">
             <img
               alt="gallery"
               src={sample}
-              className="object-cover max-w-[300px] h-[265px] lg:h-[400px]"
+              className="object-cover max-w-[300px] h-[265px] lg:h-[400px] rounded-2xl rounded-bl-none -mx-px"
             />
           </div>
-          <div className="h-fit max-h-[415px] border-8 border-primary-pink">
+          <div className="h-fit max-h-[415px] border-4 border-[#210D15] rounded-tr-3xl rounded-bl-3xl">
             <img
               alt="gallery"
               src={sample6}
-              className="object-cover max-w-[300px] h-[265px] lg:h-[400px]"
+              className="object-cover max-w-[300px] h-[265px] lg:h-[400px] rounded-tr-2xl rounded-bl-2xl -mx-px"
             />
           </div>
-          <div className="flex flex-row lg:flex-col justify-between md:max-h-[415px] gap-2">
-            <div className="border-8 border-primary-pink max-h-[280px] md:max-h-full">
+          <div className="flex flex-row lg:flex-col justify-between md:justify-normal md:max-h-[415px] gap-2">
+            <div className="border-4 border-primary-pink rounded-t-3xl max-h-[280px] md:max-h-full">
               <img
                 alt="gallery"
                 src={sample7}
-                className="object-cover max-w-sm lg:w-full h-[265px] lg:h-[180px]"
+                className="object-cover max-w-sm lg:w-full h-[265px] lg:h-[180px] rounded-t-xl"
               />
             </div>
-            <div className="border-8 border-primary-pink h-fit">
+            <div className="border-4 border-[#210D15] h-fit rounded-b-2xl">
               <img
                 alt="gallery"
                 src={sample8}
-                className="object-cover max-w-sm h-[265px] lg:h-[180px]"
+                className="object-cover max-w-sm h-[265px] lg:h-[200px] rounded-b-xl"
               />
             </div>
           </div>
