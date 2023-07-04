62a4b1984c12b1a757c2578df776c62ab488b7b6
EseAlli <essealli.ea@gmail.com>
2023-07-04 11:57:12 +0100

    added one more image and countdown component

---
+++ src/components/CountdownTimer/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useCountdown } from "../../hooks/useCountdown";
+import ShowCounter from "./ShowCounter";
+
+const CountdownTimer = ({ targetDate }) => {
+  const [days, hours, minutes, seconds] = useCountdown(targetDate);
+
+  if (days + hours + minutes + seconds <= 0) {
+    return <></>;
+  } else {
+    return (
+      <ShowCounter
+        days={days}
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
+      />
+    );
+  }
+};
+
+export default CountdownTimer;
