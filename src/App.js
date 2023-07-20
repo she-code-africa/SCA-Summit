1a98af6a776300c77d37969d2448f3aa6962f500
EseAlli <essealli.ea@gmail.com>
2023-07-20 12:36:13 +0100

    clear console

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import { useEffect } from "react";
 function App() {
   useEffect(() => {
     // Check if the current protocol is HTTP
-    console.log(process.env.NODE_ENV);
     if (process.env.NODE_ENV === "production") {
-      console.log(window.location.protocol);
       if (window.location.protocol === "http:") {
         // Redirect to the equivalent URL with HTTPS
         window.location.href = `https://${window.location.hostname}${window.location.pathname}`;
