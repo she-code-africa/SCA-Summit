076ef389eb53e95785b56fcb0b42c6d1d9f99b0e
EseAlli <essealli.ea@gmail.com>
2023-08-07 01:27:11 +0100

    extracted btn links into component

--- src/components/Anchor/index.jsx
+++ src/components/Anchor/index.jsx
@@ -3,7 +3,7 @@ import { RxArrowTopRight } from "react-icons/rx";
 export const SolidAnchor = ({ text, link }) => {
   return (
     <a
-      className="bg-primary-pink rounded-30px text-white py-3.5 px-8 hover:bg-primary-light-pink hover:text-primary-pink border border-primary-pink transition ease-in delay-75 duration-300"
+      className="bg-primary-pink rounded-30px text-white py-3.5 px-8 hover:bg-primary-light-pink hover:text-primary-pink hover:cursor-pointer border border-primary-pink transition ease-in delay-75 duration-300"
       href={link}
     >
       {text}
